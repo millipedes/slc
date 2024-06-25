0a1c3907650d36cd4534f92a98eb11440b878143
millipedes <millipedes@earthlink.net>
2024-06-25 16:14:47 -0700

    Add expression parsing abilities

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -12,13 +12,13 @@ const char * or_p(const char * input, void * data, uint64_t num_args, ...) {
   for(uint64_t i = 0; i < num_args; i++) {
     const char * remainder = va_arg(args,
         const char * (*)(const char *, void *))(input, data);
-    if(remainder != input) {
+    if(remainder != NULL) {
       va_end(args);
       return remainder;
     }
   }
   va_end(args);
-  return input;
+  return NULL;
 }
 
 const char * parse_variable_name(const char * input, void * data) {
@@ -30,8 +30,8 @@ const char * parse_variable_name(const char * input, void * data) {
     strncpy(the_expression->value.string_value, input, inc);
     the_expression->value.string_value[inc] = '\0';
     the_expression->type = VAR;
-  }
-  return input + inc;
+    return input + inc;
+  } else return NULL;
 }
 
 const char * parse_number(const char * input, void * data) {
@@ -39,34 +39,40 @@ const char * parse_number(const char * input, void * data) {
   uint64_t inc = 0;
   bool is_double = false;
 
-  while(isdigit(*(input + inc))
-      || is_double_delineator(*(input + inc))
-      || *(input + inc) == '-') {
-    if(is_double_delineator(*(input + inc))) {
-      is_double = true;
-    }
+  // decimal
+  while(isdigit(*(input + inc))) inc++;
+
+  if(*(input + inc) == '.') {
+    is_double = true;
     inc++;
+  } else if(*(input + inc) == 'e' || *(input + inc) == 'E') {
+    is_double = true;
+    inc++;
+    if(*(input + inc) == '-') inc++;
   }
+
+  // fractional
+  while(isdigit(*(input + inc))) inc++;
 
   if(is_double && inc > 0) {
     the_expression->value.double_value = strtod(input, NULL);
     the_expression->type = DOUBLE;
+    return input + inc;
   } else if(inc > 0) {
     the_expression->value.int_value = strtol(input, NULL, 10);
     the_expression->type = INT;
-  }
-
-  return input + inc;
+    return input + inc;
+  } else return NULL;
 }
 
 const char * parse_string(const char * input, void * data) {
   expression * the_expression = (expression *)data;
   uint64_t inc = 1;
   if(input[0] != '\"') {
-    return input;
+    return NULL;
   }
   while(*(input + inc) != '\"') {
-    if(*(input + inc) == '\0') return input; // not matched
+    if(*(input + inc) == '\0') return NULL;
     inc++;
   }
   inc++;
@@ -80,11 +86,65 @@ const char * parse_string(const char * input, void * data) {
 const char * parse_character(const char * input, void * data) {
   if(input[0] == *(char *)data)
     return input + 1;
-  return input;
+  return NULL;
+}
+
+const char * parse_word(const char * input, void * data) {
+  size_t len = strnlen((char *)data, MAX_STR);
+  if(!strncmp(input, (char *)data, len))
+    return input + len;
+  return NULL;
+}
+
+const char * parse_factor(const char * input, void * data) {
+  const char * remainder;
+  if((remainder = parse_character(parse_whitespace(input), (void *)"-")) != NULL) {
+    expression * parent = (expression *)data;
+    expression child = {0};
+    remainder = parse_factor(parse_whitespace(remainder), &child);
+    parent->type = UN_MINUS;
+    parent->qty_children = 1;
+    parent->child = (expression *)calloc(1, sizeof(struct EXPRESSION_T));
+    parent->child[0] = child;
+    return remainder;
+  } else if((remainder = parse_character(parse_whitespace(input), (void *)"(")) != NULL) {
+    remainder = parse_expression(parse_whitespace(remainder), data);
+    return parse_character(parse_whitespace(remainder), (void *)")");
+  } else {
+    return or_p(parse_whitespace(input), data,
+        3, parse_number, parse_string, parse_variable_name);
+  }
+}
+
+const char * parse_term(const char * input, void * data) {
+  const char * factor = parse_factor(parse_whitespace(input), data);
+  const char * maybe_term;
+  if(factor) {
+    if((maybe_term = parse_character(parse_whitespace(factor), (void *)"*")) != NULL) {
+      ADJUST_BINARY_TREE(parse_term, maybe_term, BIN_MULT);
+      return parse_character(parse_whitespace(factor), (void *)"*");
+    } else if((maybe_term = parse_character(parse_whitespace(factor), (void *)"/")) != NULL) {
+      ADJUST_BINARY_TREE(parse_term, maybe_term, BIN_DIVIDE);
+      return parse_character(parse_whitespace(factor), (void *)"/");
+    } else if((maybe_term = parse_character(parse_whitespace(factor), (void *)"%")) != NULL) {
+      ADJUST_BINARY_TREE(parse_term, maybe_term, BIN_MOD);
+      return parse_character(parse_whitespace(factor), (void *)"%");
+    } else return factor;
+  } return NULL;
 }
 
 const char * parse_expression(const char * input, void * data) {
-  return or_p(input, data, 3, parse_string, parse_number, parse_variable_name);
+  const char * term = parse_term(parse_whitespace(input), data);
+  const char * maybe_expression;
+  if(term) {
+    if((maybe_expression = parse_character(parse_whitespace(term), (void *)"+")) != NULL) {
+      ADJUST_BINARY_TREE(parse_expression, maybe_expression, BIN_PLUS);
+      return parse_character(parse_whitespace(term), (void *)"+");
+    } else if((maybe_expression = parse_character(parse_whitespace(term), (void *)"-")) != NULL) {
+      ADJUST_BINARY_TREE(parse_expression, maybe_expression, BIN_MINUS);
+      return parse_character(parse_whitespace(term), (void *)"-");
+    } else return term;
+  } return NULL;
 }
 
 bool is_double_delineator(char c) {
@@ -93,4 +153,23 @@ bool is_double_delineator(char c) {
 
 bool is_whitespace(char c) {
   return c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f';
+}
+
+void debug_expression(expression the_expression, int indent) {
+  for(int i = 0; i < 2 * indent; i++) printf(" ");
+  switch(the_expression.type) {
+    case INT:        printf("%d\n", the_expression.value.int_value);    break;
+    case DOUBLE:     printf("%f\n", the_expression.value.double_value); break;
+    case VAR:        printf("%s\n", the_expression.value.string_value); break;
+    case STRING:     printf("%s\n", the_expression.value.string_value); break;
+    case UN_MINUS:   printf("-\n");                                     break;
+    case BIN_PLUS:   printf("+\n");                                     break;
+    case BIN_MINUS:  printf("-\n");                                     break;
+    case BIN_MULT:   printf("*\n");                                     break;
+    case BIN_DIVIDE: printf("/\n");                                     break;
+    case BIN_MOD:    printf("%\n");                                     break;
+  }
+  for(uint64_t i = 0; i < the_expression.qty_children; i++) {
+    debug_expression(the_expression.child[i], indent + 1);
+  }
 }
--- src/parsing/parsing.h
+++ src/parsing/parsing.h
@@ -16,6 +16,12 @@ typedef enum {
   DOUBLE,
   VAR,
   STRING,
+  UN_MINUS,
+  BIN_PLUS,
+  BIN_MINUS,
+  BIN_MULT,
+  BIN_DIVIDE,
+  BIN_MOD,
 } expression_type;
 
 typedef struct EXPRESSION_T {
@@ -24,7 +30,7 @@ typedef struct EXPRESSION_T {
     double double_value;
     char * string_value;
   } value;
-  struct EXPRESSION_T * expression;
+  struct EXPRESSION_T * child;
   uint64_t qty_children;
   expression_type type;
 } expression;
@@ -36,9 +42,40 @@ const char * or_p(const char * input, void * data, uint64_t num_args, ...);
 const char * parse_variable_name(const char * input, void * data);
 const char * parse_number(const char * input, void * data);
 const char * parse_string(const char * input, void * data);
+const char * parse_character(const char * input, void * data);
+const char * parse_word(const char * input, void * data);
+
+#define ADJUST_BINARY_TREE(right_p, str, parent_type)                    \
+  expression parent = {0};                                               \
+  expression * left = (expression *)data;                                \
+  expression right = {0};                                                \
+  str = right_p(parse_whitespace(str), &right);                          \
+  if(str) {                                                              \
+    parent.type = parent_type;                                           \
+    parent.qty_children = 2;                                             \
+    parent.child = (expression *)calloc(2, sizeof(struct EXPRESSION_T)); \
+    parent.child[0] = *left;                                             \
+    parent.child[1] = right;                                             \
+    *(expression *)data = parent;                                        \
+    return str;                                                          \
+  }
+
+const char * parse_factor(const char * input, void * data);
+const char * parse_term(const char * input, void * data);
 const char * parse_expression(const char * input, void * data);
 
 bool is_double_delineator(char c);
 bool is_whitespace(char c);
+void debug_expression(expression the_expression, int indent);
+
+typedef struct LAMBDA_T {
+  expression arg;
+  union {
+    expression expr;
+    struct LAMBDA_T * func;
+  } function;
+} lambda;
+
+const char * parse_lambda_expression(const char * input, void * data);
 
 #endif
